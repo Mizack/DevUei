32ff50c29dfa1b6469a6b6d4f6e4da777ff4c543
mizack mianuti <mizack1234@gmail.com>
2022-11-13 16:16:04 -0300

    esses arquivos podem ser desconsiderados e excluídos. Adicionei apenas para fazer um teste

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ var express = require("express")
 const app = express()
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+"/templates/index.html")
-})
-app.get('/sobre',function(req,res){
     res.send('testando rotas')
-    // res.sendFile("C:/Users/mizac/Documents/projeto_node/aula_4/index.html")
 })
-// rotas dinamicas com parametros
-// sends são enviados uma unica vez
-app.get('/ola/:cargo/:nome',function(req,res){
-    res.send(req.params.nome+req.params.cargo)
-})
+
 
 // iniciando servidor e indicando a porta
 // nada que compoem a aplicação pode ficar abaixo do app.listem()
