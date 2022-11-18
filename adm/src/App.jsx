1cb1e6b16b5ac12000c50c42cc0128f16bfff421
Nicolas Penante <nicolas.penante10@hotmail.com>
2022-11-17 21:09:31 -0300

    Adms login page

--- adm/src/App.jsx
+++ adm/src/App.jsx
@@ -1,4 +1,28 @@
-export function App() {
-  return <h1>App</h1>
-}
+import styles from './App.module.css'
 
+export function App() {
+  return (
+    <>
+      <header className={styles.header}>
+        &#91;&#41;
+      </header>
+      <div className={styles.App}>
+        <span className={styles.logo}>&#91;&#41;EVUEI</span>
+        <form>
+          <section className={styles.loginCard}>
+            <p>Login</p>
+            <div>
+              <input type="email" placeholder='Digite seu e-mail...'/>
+              <input type="password" placeholder='Digite sua senha...'/>
+            </div>
+            <footer>
+              <button type="submit">
+                Entrar
+              </button>
+            </footer>
+          </section>
+        </form>
+      </div>
+    </>
+  )
+}
